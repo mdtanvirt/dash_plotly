64dfab83ee468dcc12380478e0c1cf439b354e2f
Tanvir Rahman <trahman@paradynamix.com>
2022-10-02 22:49:10 +0600

    chart initiate

--- app.py
+++ app.py
@@ -1,24 +1,58 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
+df = pd.read_csv("data/stockdata2.csv", index_col=0, parse_dates=True)
+df.index = pd.to_datetime(df['Date'])
+
+# Initialize app
 app = Dash(__name__)
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+def get_options(list_stocks):
+    dict_list = []
+    for i in list_stocks:
+        dict_list.append({'label': i, 'value': i})
 
-fig = px.scatter(df, x = "gdp per capita", y = "life expectancy",
-                    size = "population", color = "continent", hover_name= "country",
-                    log_x=True, size_max=60)
+    return dict_list
 
-app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gap',
-        figure=fig
-    )
-])
+# Define the app
+app.layout = html.Div(
+    children=[
+        html.Div(className='row', # Define the row elements
+            children=[
+                html.Div(className='four columns div-user-controls', # Define the left element
+                children = [
+                        html.H2('Dash - STOCK PRICES'),
+                        html.P('''Visualising time series with Plotly - Dash'''),
+                        html.P('''Pick one or more stocks from the dropdown below.'''),
+                        html.Div(className='div-for-dropdown',
+                            children=[
+                                dcc.Dropdown(id='stockselector',
+                                            options=get_options(df['stock'].unique()),
+                                            multi=True,
+                                            value=[df['stock'].sort_values()[0]],
+                                            style={'backgroundColor': '#1E1E1E'},
+                                            className='stockselector')
+                                        ],
+                            style={'color': '#1E1E1E'})
+                    ]), 
+                html.Div(className='eight columns div-for-charts bg-grey', # Define the right element
+                children= [
+                    dcc.Graph(id='timeseries',
+                    config={'displayModeBar': False},
+                    animate=True,
+                    figure=px.line(df,
+                                    x='Date',
+                                    y='value',
+                                    color='stock',
+                                    template='plotly_dark').update_layout(
+                                            {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                                                'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+                            )
+                ]) 
+                ])
+    ]) 
 
-if __name__ == '__main__':
+# Run the app
+if __name__ == "__main__":
     app.run_server(debug=True)
